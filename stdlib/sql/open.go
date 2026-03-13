2e65ff25f292144b2868d75c679c4de7f006498f
Martin Hilton <mhilton@influxdata.com>
2026-03-13 13:00:38 +0000

    feat: use custom dialer in snowflake connections
    When connecting to a snowflake database use an http.Transport defived
    from the provided dependencies.

--- stdlib/sql/open.go
+++ stdlib/sql/open.go
@@ -42,6 +42,8 @@ func getOpenFunc(driverName, dataSourceName string) openFunc {
 		return mssqlOpenFunction(driverName, dataSourceName)
 	case "postgres":
 		return postgresOpenFunction(dataSourceName)
+	case "snowflake":
+		return snowflakeOpenFunction(dataSourceName)
 	default:
 		return defaultOpenFunction(driverName, dataSourceName)
 	}
